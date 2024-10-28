0656cbd2107019d12042454def4e8c2dbc69f796
Ricky109 <114699211+Ricky109@users.noreply.github.com>
2024-10-27 23:01:04 -0500

    SQL_Crear_BD.sql
    último

--- SQL_Crear_BD.sql
+++ SQL_Crear_BD.sql
@@ -134,7 +134,7 @@ CREATE TABLE DetalleFormatoHorasExtra (
 )engine=InnoDB;
 
 CREATE INDEX idx_cargo_trabajador ON Trabajador (cargo);
-CREATE INDEX idx_nombre_apellido_trabajador ON Trabajador (nombre, apellidos);
+CREATE INDEX idx_nombre_apellido_trabajador ON Trabajador (nombres, apellidos);
 CREATE INDEX idx_nombre_apellido_funcionario ON Funcionario (nombre, apellidos);
 CREATE INDEX idx_telefono ON Telefono (telefono);
 CREATE INDEX idx_fecha_reporte_diario ON ReporteDiario (fecha);
