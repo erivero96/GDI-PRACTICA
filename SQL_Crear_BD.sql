de1941554625fab4bf26bb50a252ece795255c93
Ricky109 <114699211+Ricky109@users.noreply.github.com>
2024-10-27 22:47:18 -0500

    SQL_Crear_BD.sql
    agregando índices

--- SQL_Crear_BD.sql
+++ SQL_Crear_BD.sql
@@ -132,3 +132,9 @@ CREATE TABLE DetalleFormatoHorasExtra (
     FOREIGN KEY (idFormatoHorasExtra) REFERENCES FormatoHorasExtra(idFormatoHorasExtras),
     foreign key (dni, fecha) references EquivalenteEnHoras (dni, fecha)
 )engine=InnoDB;
+
+CREATE INDEX idx_cargo_trabajador ON Trabajador (cargo);
+CREATE INDEX idx_nombre_apellido_trabajador ON Trabajador (nombre, apellidos);
+CREATE INDEX idx_nombre_apellido_funcionario ON Funcionario (nombre, apellidos);
+CREATE INDEX idx_telefono ON Telefono (telefono);
+CREATE INDEX idx_fecha_reporte_diario ON ReporteDiario (fecha);
